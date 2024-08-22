18bde2765830b2e81f33974c87807ac5b6c17268
Yeldos <s.yeldos@yandex.kz>
2024-08-22 14:27:47 +0500

    django-full

--- kaspi/models.py
+++ kaspi/models.py
@@ -1,11 +1,19 @@
+import datetime
+
 from django.db import models
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save, post_delete
 
+from django.contrib.postgres.fields import DateTimeRangeField, ArrayField
 
 class Good(models.Model):
     name = models.CharField(max_length=100)
     discription = models.CharField(max_length=100)
     price = models.IntegerField()
+    reserving = DateTimeRangeField(verbose_name='Reserving time', default=(datetime.datetime.now(), '2024-12-31'))
+    array_field = ArrayField(base_field=models.CharField(max_length=10), default=[])
+
 
     def __str__(self):
         return self.name
@@ -28,3 +36,15 @@ class MagazineGood(models.Model):
 
     def __str__(self):
         return f'{self.magazine.name} = {self.good.name}'
+
+
+# Signal
+@receiver(post_save, sender=Good)
+def post_save_dispatcher(sender, **kwargs):
+    data = kwargs['instance']
+    print(f'{data.name} is created!')
+
+@receiver(post_delete, sender=Good)
+def post_delete_dispatcher(sender, **kwargs):
+    data = kwargs['instance']
+    print(f'{data.name} is deleted!')
